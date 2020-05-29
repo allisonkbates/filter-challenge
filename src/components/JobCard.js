c4c509dc822b95736a57f43fba90e8569fd59ff5
Allison Bates <allisonkbates@gmail.com>
2020-05-29 18:51:19 -0400

    finally got the images dynamic

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import StaticInfo from '../components/StaticInfo';
 import TagsList from '../components/TagsList';
-import Account from '../images/account.svg'; //change to bring in dynamic images
 import './JobCard.css';
 
 
 class JobCard extends Component {
   render() {
+    const iconPath = process.env.PUBLIC_URL;
     const posting = this.props.posting;
     return (
       <div className="JobCard">
-        <img src={Account} alt={posting.company} width="87" height="87"></img>
+        <img src={iconPath + posting.logo} alt={posting.company} width="87" height="87"></img>
         <StaticInfo posting={posting}/>
         <TagsList posting={posting} handleClick={this.props.handleClick}/>
       </div>
